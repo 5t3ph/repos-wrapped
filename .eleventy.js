2e28051144e819024bf704c93743ce53d030888a
5t3ph <thinkdobecreate@gmail.com>
2021-12-28 15:45:36 -0600

    ✨ Soft launch

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,33 @@
+const { DateTime } = require("luxon");
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "stats",
+    functionsDir: "./netlify/functions/",
+  });
+
+  eleventyConfig.addShortcode("mostStarred", (repos) => {
+    const originalRepos = [...repos];
+    const repo = originalRepos.sort((a, b) => {
+      return b.stargazers_count - a.stargazers_count;
+    })[0];
+
+    return `<a href="${repo.html_url}">${repo.name}</a>`;
+  });
+
+  eleventyConfig.addFilter("lastUpdated", (repos) => {
+    return repos.sort((a, b) => {
+      new Date(a.updated_at) - new Date(b.updated_at);
+    })[0].updated_at;
+  });
+
+  eleventyConfig.addFilter("repoDate", (date) => {
+    const d = DateTime.fromISO(date);
+    return d.toFormat("LLL d, yyyy");
+  });
 
   return {
     dir: {
