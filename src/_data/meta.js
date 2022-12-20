436cd863596701a03780f711f27976492725c702
5t3ph <thinkdobecreate@gmail.com>
2022-12-19 22:38:33 -0600

    Update to dynamic currentYear

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,4 +2,5 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "repos wrapped",
   description: "A yearly review of your public GitHub repository stats",
+  currentYear: new Date().getFullYear(),
 };
